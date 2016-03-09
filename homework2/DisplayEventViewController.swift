145872a4a90eabcd0ce723aaa1d279db954ff485
Rebecca Hajduk <rebeccahajduk@rebeccas-macbook-pro-2.local>
2016-03-09 14:08:50 -0600

    Added view controller classes and a segue

---
+++ homework2/DisplayEventViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DisplayEventViewController.swift
+//  homework2
+//
+//  Created by Rebecca Hajduk on 3/9/16.
+//  Copyright © 2016 Rebecca Hajduk. All rights reserved.
+//
+
+import Cocoa
+
+class DisplayEventViewController: UIViewController {
+
+}
---
+++ homework2/EventsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EventsTableViewController.swift
+//  homework2
+//
+//  Created by Rebecca Hajduk on 3/9/16.
+//  Copyright © 2016 Rebecca Hajduk. All rights reserved.
+//
+
+import Cocoa
+
+class EventsTableViewController: UITableViewController {
+
+}
